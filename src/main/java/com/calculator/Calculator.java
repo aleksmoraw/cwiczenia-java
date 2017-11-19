53c2a4a3a3d7549b2ae2bd8317e542ef374d3e7c
aleksmoraw <aleksandra_morawska@onet.pl>
2017-11-19 16:06:57 +0100

    refactor Calculator class

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -3,23 +3,19 @@ package com.calculator;
 public class Calculator {
 
     public double add (double a, double b) {
-        double result;
-        return result = a + b;
+        return a + b;
     }
 
     public double subtract (double a, double b) {
-        double result;
-        return result = a - b;
+        return a - b;
     }
 
     public double multiply (double a, double b) {
-        double result;
-        return result = a * b;
+        return a * b;
     }
 
     public double divide (double a, double b) {
-        double result;
-        return result = a / b;
+        return a / b;
     }
 }
 
