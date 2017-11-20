9ac004e145fa301b53b344d22bf000398337a5e8
aleksmoraw <aleksandra_morawska@onet.pl>
2017-11-20 08:36:46 +0100

    refactor in PointControllerTest class

--- src/test/java/com/point/PointControllerTest.java
+++ src/test/java/com/point/PointControllerTest.java
@@ -9,31 +9,28 @@ import static org.junit.Assert.assertTrue;
 public class PointControllerTest {
 
     PointController pointController = new PointController();
+    private Point point = new Point(20, 50);
 
     @Test
     public void isAddXisCorrect () {
-        Point point = new Point(20, 50);
         Integer sut = pointController.addX(point).getX();
         assertTrue(sut.equals(21));
     }
 
     @Test
     public void isMinusXisCorrect () {
-        Point point = new Point(20, 50);
         Integer sut = pointController.minusX(point).getX();
         assertTrue(sut.equals(19));
     }
 
     @Test
     public void isAddYisCorrect () {
-        Point point = new Point(20, 50);
         Integer sut = pointController.addY(point).getY();
         assertTrue(sut.equals(51));
     }
 
     @Test
     public void isMinusYisCorrect () {
-        Point point = new Point(20, 50);
         Integer sut = pointController.minusY(point).getY();
         assertTrue(sut.equals(49));
     }
