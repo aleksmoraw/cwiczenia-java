de09436336d648cae748d7acc6cee8e061bb6814
aleksmoraw <aleksandra_morawska@onet.pl>
2017-11-20 08:46:54 +0100

    add switch case in PointApplication

--- src/main/java/point/PointApplication.java
+++ src/main/java/point/PointApplication.java
@@ -2,11 +2,34 @@ package point;
 
 public class PointApplication {
 
+    public static final int ADD_X = 0;
+    public static final int ADD_Y = 1;
+    public static final int MINUS_X = 2;
+    public static final int MINUS_Y = 3;
+
     public static void main(String[] args) {
 
         PointController pointController = new PointController();
         Point point = new Point(10,20);
-        pointController.addX(point);
+
+        int option = ADD_X;
+
+        switch (option) {
+            case ADD_X:
+                pointController.addX(point);
+                break;
+            case ADD_Y:
+                pointController.addY(point);
+                break;
+            case MINUS_X:
+                pointController.minusX(point);
+                break;
+            case MINUS_Y:
+                pointController.minusY(point);
+                break;
+            default:
+                System.out.println("Podano błędność wartość.");
+        }
 
         System.out.println("Współrzędne po zmianie: " + point.getX() + " " + point.getY());
     }
